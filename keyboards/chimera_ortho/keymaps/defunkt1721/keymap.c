90c60c480a9af5d2fab07cf3d81005c16971506b
Aleksandar Zhelyazov <azhelyazov@llxgbs.com>
2018-03-27 17:37:18 +0100

    Add bracket layer

--- keyboards/chimera_ortho/keymaps/defunkt1721/keymap.c
+++ keyboards/chimera_ortho/keymaps/defunkt1721/keymap.c
@@ -7,11 +7,11 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-enum chimera_ortho_layers
-{
-  _DVORAK,
+enum chimera_ortho_layers {
+  _DVORAK = 0,
   _SYMBOLS,
-  _NAV
+  _NAV,
+  _BRACKETS
 };
 
 //Tap Dance Declarations
@@ -25,12 +25,13 @@ enum {
 // Layer toggles
 #define KC_SPNV LT(_NAV,KC_SPC)
 #define KC_SPSM LT(_SYMBOLS, KC_SPC)
+#define KC_ENTBR LT(_BRACKETS, KC_ENT)
 
 // Mod toggles
 #define KC_ALDL MT(MOD_RALT, KC_DEL)
 #define KC_ALTM MT(MOD_LALT, KC_MINS)
 #define KC_TBCL MT(MOD_LCTL, KC_TAB)
-#define KC_MICL MT(MOD_RCTL, KC_MINS)
+#define KC_SLCL MT(MOD_RCTL, KC_SLSH)
 #define KC_GUIDEL MT(MOD_LGUI, KC_DEL)
 #define KC_SFENT MT(MOD_RSFT, KC_ENT)
 
@@ -67,23 +68,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = KC_KEYMAP(
   //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-    GESC,QUOT, COMM, DOT, P , Y   ,ALTM,      ALDL, F  , G  , C  , R  , L  ,SLSH,
+    GESC,QUOT, COMM, DOT, P , Y   ,ALTM,      ALDL, F  , G  , C  , R  , L  ,BSPC,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-    TBCL, A  , O  , E  , U  , I   ,LBRA,      RBRA, D  , H  , T  , N  , S  ,MICL,
+    TBCL, A  , O  , E  , U  , I   ,LBRA,      RBRA, D  , H  , T  , N  , S  ,SLCL,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-   SHCPS, CLNS, Q , J  , K  , X   ,LPRN,      RPRN, B  , M  , W  , V  , Z ,SFENT,
+   SHCPS, SCLN, Q , J  , K  , X   ,LPRN,      RPRN, B  , M  , W  , V  , Z  ,RSFT,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                      GUIDEL,SPNV,                SPSM, BSPC
+                      GUIDEL,SPNV,                SPSM, ENTBR
   // \------------------+----+----+---/       \---+----+----+-------------------/
   ),
 
   [_NAV] = KC_KEYMAP(
   //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-        ,XXXX,XXXX, UP ,XXXX,XXXX,XXXX,       XXXX,XXXX,XXXX,PGUP,XXXX,FNDE,XXXX,
+        ,XXXX,XXXX, UP ,XXXX,XXXX,XXXX,       PSCR,XXXX,XXXX,PGUP,XXXX,FNDE,INS ,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
         ,XXXX,LEFT,DOWN,RGHT,XXXX,XXXX,       XXXX,XXXX,HOME,PGDN,END, CTE ,UNDO,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-        ,XXXX,XXXX,XXXX,XXXX,XXXX,PSCR,       INS,XXXX,XXXX,XXXX, XXXX,CPY ,PTE ,
+        ,XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,       XXXX,XXXX,XXXX,XXXX,XXXX,CPY ,PTE ,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
                              XXXX,XXXX,             LALT,LCTL
   // \------------------+----+----+---/       \---+----+----+-------------------/
@@ -93,11 +94,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
     GRV , QUOT,COMM,DOT , F4 , F5 , F11,        F12 , F6 , F7 , F8 , F9,F10,QUES,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-        ,   1 ,  2 ,  3 ,  4 ,  5 ,BSLS,      SLSH,  6 ,  7 ,  8 ,  9 ,  0 ,EQL ,
+        ,   1 ,  2 ,  3 ,  4 ,  5 ,BSLS,      SLSH,  6 ,  7 ,  8 ,  9 ,  0 ,MINS,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-        ,EXLM,  AT ,HASH, DLR,PERC,MINS,      PLUS,CIRC,AMPR,ASTR,LPRN,RPRN,    ,
+        ,EXLM,  AT ,HASH, DLR,PERC,MINS,      PLUS,CIRC,AMPR,ASTR,LPRN,RPRN,EQL,
   //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                            ,    ,                    ,RSPC
+                            ,LALT ,                    ,SPC
+  // \------------------+----+----+---/       \---+----+----+-------------------/
+  ),
+
+  [_BRACKETS] = KC_KEYMAP(
+  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
+        ,QUOT, DOT,LPRN,RPRN,AMPR,PIPE,      XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+        ,GRV , DLR,LCBR,RCBR,EQL ,COLN,      XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+        ,BSLS,SLSH,LABK,RABK,EXLM,MINS,      PLUS,XXXX,XXXX,XXXX,XXXX,XXXX,    ,
+  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
+                            ,    ,                    ,
   // \------------------+----+----+---/       \---+----+----+-------------------/
   )
 };
