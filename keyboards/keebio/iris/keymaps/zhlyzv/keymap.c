5fa2dfc9cf7fefc5a7a79c8caf024097e8f66fac
Alexander Zhelyazov <azhelyazov@gmail.com>
2019-05-25 21:22:08 +0100

    add keymap to keebio folder

---
+++ keyboards/keebio/iris/keymaps/zhlyzv/keymap.c
@@ -0,0 +1,145 @@
+#include "iris.h"
+#include "action_layer.uaaoeuh"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+#define _DVORAK 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
+
+enum custom_keycodes {
+  DVORAK = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+// Fillers to make layering more clear
+#define ____ KC_TRNS
+#define KC_XXXX KC_NO
+#define KC_ KC_TRNS
+
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
+
+// RGB keycodes
+#define KC_RTOG RGB_TOG
+#define KC_RMOD RGB_MOD
+#define KC_RRMOD RGB_RMOD
+#define KC_RHUI RGB_HUI
+#define KC_RHUD RGB_HUD
+#define KC_RSAI RGB_SAI
+#define KC_RSAD RGB_SAD
+#define KC_RVAI RGB_VAI
+#define KC_RVAD RGB_VAD
+
+#define KC_TBCL MT(MOD_LCTL, KC_TAB)
+#define KC_MICL MT(MOD_LCTL, KC_MINS)
+#define KC_SCLG MT(MOD_LGUI, KC_SCLN)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_DVORAK] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     ESC , 1  , 2  , 3  , 4  ,  5 ,                6  , 7  , 8  , 9  , 0  ,BSPC,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     GRV ,QUOT,COMM, DOT, P ,  Y  ,                F  , G  , C  , R  , L,  SLSH,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     TBCL, A  , O  , E  , U  , I  ,                D  , H  , T  , N  , S , MICL,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+     LSFT,SCLG, Q , J  , K  , X  ,SPC ,     ENT ,  B  , M  , W  , V  , Z , RSFT,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                       LALT,LOWR,SPC ,         ENT ,RASE,RALT
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_LOWER] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+         ,XXXX,XXXX,XXXX,XXXX,XXXX,               XXXX,XXXX,XXXX,XXXX,XXXX,DEL ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,XXXX,XXXX, UP ,XXXX,XXXX,               XXXX,XXXX,PGUP,XXXX,XXXX,XXXX,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,XXXX,LEFT,DOWN,RGHT,XXXX,               XXXX,HOME,PGDN,END ,XXXX,    ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,    ,XXXX,XXXX,XXXX,XXXX,XXXX,     XXXX,XXXX,XXXX,XXXX,XXXX,XXXX,    ,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                            ,    ,XXXX,       XXXX,    ,LCTL
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_RAISE] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,GRV ,DLR ,LCBR,RCBR,    ,               TILD,PIPE,AMPR,COLN,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,LBRC,RBRC,LPRN,RPRN,    ,               NUHS,EXLM, EQL,SCLN,    ,    ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,BSLS,SLSH,LABK,RABK,    ,    ,         ,NUBS,PLUS,MINS,UNDS,    ,    ,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,             ,    ,
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+  [_ADJUST] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,                   ,MUTE,MSTP,MPLY,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+     RTOG,RMOD,RHUI,RSAI,RVAI,    ,                   ,    ,VOLU,    ,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+        ,RRMOD,RHUD,RSAD,RVAD,    ,                    ,MPRV,VOLD,MNXT,    ,    ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,    ,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,             ,    ,
+  //                  `----+----+----'        `----+----+----'
+  )
+
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case DVORAK:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_DVORAK);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
